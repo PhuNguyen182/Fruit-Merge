e9850168a144ba8b44685b10bf46a2edf958bcf8
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-06-07 23:21:41 +0700

    Add game state machine

--- Assets/_FruitMerge/Scripts/Gameplay/GameManagement/GameController.cs
+++ Assets/_FruitMerge/Scripts/Gameplay/GameManagement/GameController.cs
@@ -1,3 +1,4 @@
+using _FruitMerge.Scripts.Gameplay.GameManagement.StateMachine;
 using UnityEngine;
 
 namespace _FruitMerge.Scripts.Gameplay.GameManagement
@@ -6,7 +7,8 @@ namespace _FruitMerge.Scripts.Gameplay.GameManagement
     {
         [SerializeField] private FruitSpawner fruitSpawner;
         [SerializeField] private FruitDragController fruitDragController;
-        
+
+        private GameStateController _gameStateController;
         private MessageBrokerManager _messageBrokerManager;
         
         private void Awake()
@@ -14,10 +16,23 @@ namespace _FruitMerge.Scripts.Gameplay.GameManagement
             this.Initialize();
         }
 
+        private void Start()
+        {
+            this.StartGame();
+        }
+
+        #region Initialization
+
         private void Initialize()
         {
             this.InitializeMessageBroker();
+            this.InitializeGameStateMachine();
             this.InitializeFruitMergeGame();
+        }
+
+        private void InitializeGameStateMachine()
+        {
+            this._gameStateController = new GameStateController();
         }
 
         private void InitializeMessageBroker()
@@ -30,6 +45,13 @@ namespace _FruitMerge.Scripts.Gameplay.GameManagement
         {
             this.fruitSpawner.InitializeFruitSpawner();
             this.fruitDragController.InitializeFruitDragController();
+        }
+        
+        #endregion
+
+        private void StartGame()
+        {
+            this._gameStateController.PlayGame();
         }
     }
 }
---
+++ Assets/_FruitMerge/Scripts/Gameplay/GameManagement/StateMachine/GameStateController.cs
@@ -0,0 +1,94 @@
+using Stateless;
+
+namespace _FruitMerge.Scripts.Gameplay.GameManagement.StateMachine
+{
+    public class GameStateController
+    {
+        private StateMachine<GameState, StateTrigger> _gameStateMachine;
+
+        public GameStateController()
+        {
+            this.BuildGameStateMachine();
+        }
+
+        private void BuildGameStateMachine()
+        {
+            this._gameStateMachine = new StateMachine<GameState, StateTrigger>(GameState.Begin);
+            this._gameStateMachine.Configure(GameState.Begin)
+                .OnActivate(this.OnStateMachineActivate);
+            
+            this._gameStateMachine.Configure(GameState.Playing)
+                .Permit(StateTrigger.EndGame, GameState.EndGame)
+                .Permit(StateTrigger.QuitGame, GameState.Quited)
+                .OnEntryFrom(StateTrigger.PlayGame, this.OnPlayGame)
+                .OnEntryFrom(StateTrigger.ContinuePlayGame, this.OnContinuePlayGame);
+
+            this._gameStateMachine.Configure(GameState.EndGame)
+                .Permit(StateTrigger.ContinuePlayGame, GameState.Playing)
+                .Permit(StateTrigger.QuitGame, GameState.Quited)
+                .OnEntryFrom(StateTrigger.EndGame, this.OnEndGame);
+
+            this._gameStateMachine.Configure(GameState.EndGame)
+                .OnEntryFrom(StateTrigger.QuitGame, this.OnQuitGame);
+            
+            this._gameStateMachine.Activate();
+        }
+
+        #region State Machine Callbacks
+        
+        private void OnStateMachineActivate()
+        {
+            
+        }
+
+        private void OnPlayGame()
+        {
+            
+        }
+        
+        private void OnContinuePlayGame()
+        {
+            
+        }
+
+        private void OnEndGame()
+        {
+            
+        }
+
+        private void OnQuitGame()
+        {
+            
+        }
+        
+        #endregion
+
+        #region State Machine Trigger Functions
+
+        public void PlayGame()
+        {
+            if (this._gameStateMachine.CanFire(StateTrigger.PlayGame))
+                this._gameStateMachine.Fire(StateTrigger.PlayGame);
+        }
+
+        public void EndGame()
+        {
+            if (this._gameStateMachine.CanFire(StateTrigger.EndGame))
+                this._gameStateMachine.Fire(StateTrigger.EndGame);
+        }
+
+        public void ContinueGame()
+        {
+            if (this._gameStateMachine.CanFire(StateTrigger.ContinuePlayGame))
+                this._gameStateMachine.Fire(StateTrigger.ContinuePlayGame);
+        }
+
+        public void QuitGame()
+        {
+            if (this._gameStateMachine.CanFire(StateTrigger.QuitGame))
+                this._gameStateMachine.Fire(StateTrigger.QuitGame);
+        }
+
+        #endregion
+    }
+}
