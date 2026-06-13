567e7ff925e8183b928efc69c633ef625677b534
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-06-13 15:21:44 +0700

    Add camera spec

--- Assets/_FruitMerge/Scripts/Gameplay/GameTask/HammerBoosterTask.cs
+++ Assets/_FruitMerge/Scripts/Gameplay/GameTask/HammerBoosterTask.cs
@@ -2,6 +2,7 @@ using _FruitMerge.Scripts.Gameplay.GameEntity.FruitEntity;
 using _FruitMerge.Scripts.Gameplay.GameEntity.FruitEntity.Messages;
 using _FruitMerge.Scripts.Input;
 using MessagePipe;
+using UnityEngine;
 
 namespace _FruitMerge.Scripts.Gameplay.GameTask
 {
@@ -45,7 +46,14 @@ namespace _FruitMerge.Scripts.Gameplay.GameTask
             this._inputController.IsInputActive = false;
             this.ShowFruitBoosterOutline(false);
             this._pickedFruitItem.ForceBreakFruit();
-            this._cameraVibratePublisher.Publish(new CameraVibrateMessage());
+            this._cameraVibratePublisher.Publish(new CameraVibrateMessage
+            {
+                Duration = 0.2f,
+                Amplitude = 6,
+                Frequency = 6,
+                ImpulseVelocity = new Vector3(-0.15f, 0.15f, 0.5f),
+            });
+            
             this._pickedFruitItem = null;
             this._inputController.IsInputActive = true;
         }
