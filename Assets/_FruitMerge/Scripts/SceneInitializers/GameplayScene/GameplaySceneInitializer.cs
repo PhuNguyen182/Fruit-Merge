a7bd78d3c7a2504819226666d6daf6f3dfa5fbf7
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2026-06-14 22:31:48 +0700

    Fix camera and null gameplay reference, add canvas safe area

--- Assets/_FruitMerge/Scripts/SceneInitializers/GameplayScene/GameplaySceneInitializer.cs
+++ Assets/_FruitMerge/Scripts/SceneInitializers/GameplayScene/GameplaySceneInitializer.cs
@@ -1,18 +1,22 @@
-using System;
-using Cysharp.Threading.Tasks;
 using _FruitMerge.Scripts.Gameplay.GameEntity.Camera;
 using _FruitMerge.Scripts.Gameplay.GameManagement;
-using _FruitMerge.Scripts.Gameplay.GameTask;
 using DracoRuan.CoreSystems.AssetBundleSystem.Runtime;
 using DracoRuan.Foundation.UISystem.Canvases;
 using DracoRuan.Foundation.UISystem.Popups.PopupManager;
 using ServiceLocators.Core;
+using Unity.Cinemachine;
 using UnityEngine;
 
 namespace _FruitMerge.Scripts.SceneInitializers.GameplayScene
 {
     public class GameplaySceneInitializer : MonoBehaviour
     {
+        private const float DefaultCameraSize = 10f;
+        private const float StandardScreenRatio = 9f / 16f;
+        
+        [SerializeField] private Camera mainCamera;
+        [SerializeField] private CinemachineCamera viewCamera;
+        [SerializeField] private FruitMergeGameController fruitMergeGameController;
         [SerializeField] private CameraShakeController cameraShakeController;
         [SerializeField] private PopupCollection popupCollection;
         [SerializeField] private UICanvasManager canvasManager;
@@ -27,7 +31,12 @@ namespace _FruitMerge.Scripts.SceneInitializers.GameplayScene
         private void Awake()
         {
             this.InitializeAssetBundleService();
-            this.InitializeFruitMergeGame().Forget();
+            this.RecalculateCameraSizeByScreenRatio();
+        }
+
+        private void Start()
+        {
+            this.InitializeFruitMergeGame();
         }
 
         private void InitializeAssetBundleService()
@@ -37,18 +46,18 @@ namespace _FruitMerge.Scripts.SceneInitializers.GameplayScene
             this._popupManager = new UIPopupManager(this.popupCollection);
         }
 
-        private async UniTaskVoid InitializeFruitMergeGame()
+        private void InitializeFruitMergeGame()
         {
-            GameObject fruitMergeGamePrefab =
-                await this._assetBundleService.AssetBundleLoader.LoadAsset("FruitMergeGame");
-            if (fruitMergeGamePrefab)
+            this.fruitMergeGameController.InitializeGame();
+        }
+
+        private void RecalculateCameraSizeByScreenRatio()
+        {
+            float currentCameraRatio = this.mainCamera.aspect;
+            if (currentCameraRatio < StandardScreenRatio)
             {
-                GameObject fruitMergeGame = Instantiate(fruitMergeGamePrefab);
-                if (fruitMergeGame &&
-                    fruitMergeGame.TryGetComponent(out FruitMergeGameController fruitMergeGameController))
-                {
-                    fruitMergeGameController.InitializeGame();
-                }
+                float scale = StandardScreenRatio / currentCameraRatio;
+                this.viewCamera.Lens.OrthographicSize = scale * DefaultCameraSize;
             }
         }
     }
