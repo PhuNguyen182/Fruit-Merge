6575d6eb85f084213786dc0d234015058dc27f6e
PhuNguyen <nguyenhuuphu2000.a@gmail.com>
2026-07-26 23:44:55 +0700

    Add lose sound

--- Assets/_FruitMerge/Scripts/Gameplay/UI/Popups/LosePopup.cs
+++ Assets/_FruitMerge/Scripts/Gameplay/UI/Popups/LosePopup.cs
@@ -1,4 +1,5 @@
 using Constants;
+using GlobalScripts.Audios;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -8,10 +9,16 @@ namespace _FruitMerge.Scripts.Gameplay.UI.Popups
     public class LosePopup : MonoBehaviour
     {
         [SerializeField] private Button replayButton;
+        [SerializeField] private AudioClip loseSound;
 
         private void Awake()
         {
             this.replayButton.onClick.AddListener(this.Replay);
+        }
+
+        private void OnEnable()
+        {
+            AudioManager.Instance.PlayFloatingAudio(this.loseSound);
         }
 
         private void Replay()
