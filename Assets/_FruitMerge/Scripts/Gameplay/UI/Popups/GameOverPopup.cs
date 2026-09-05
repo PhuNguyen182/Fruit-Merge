043413e98c28b6f3cc66184d0b16e93eb1dd9c3e
PhuNguyen <nguyenhuuphu2000.a@gmail.com>
2026-09-05 17:38:10 +0700

    Add fake names and game over popup

--- Assets/_FruitMerge/Scripts/Gameplay/UI/Popups/GameOverPopup.cs
+++ Assets/_FruitMerge/Scripts/Gameplay/UI/Popups/GameOverPopup.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using Constants;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using _FruitMerge.Scripts.Gameplay.GameManagement;
+using _FruitMerge.Scripts.Gameplay.UI.Misc;
+using TMPro;
+using Random = UnityEngine.Random;
 
 namespace _FruitMerge.Scripts.Gameplay.UI.Popups
 {
@@ -10,11 +14,29 @@ namespace _FruitMerge.Scripts.Gameplay.UI.Popups
     {
         [SerializeField] private Button normalReplayButton;
         [SerializeField] private Button randomReplayButton;
+        [SerializeField] private List<TMP_Text> playerRanks;
+        [SerializeField] private FakeNameCollection fakeNames;
 
         private void Awake()
         {
             this.normalReplayButton.onClick.AddListener(this.NormalReplay);
             this.randomReplayButton.onClick.AddListener(this.RandomReplay);
+        }
+
+        private void OnEnable()
+        {
+            this.ShowFakeRankers();
+        }
+
+        private void ShowFakeRankers()
+        {
+            int count = this.playerRanks.Count;
+            for (int i = 0; i < count; i++)
+            {
+                int randomNumber = Random.Range(1, 1000);
+                string rankerName = this.fakeNames.GetRandomName();
+                this.playerRanks[i].text = $"{rankerName}{randomNumber}";
+            }
         }
 
         private void NormalReplay()
